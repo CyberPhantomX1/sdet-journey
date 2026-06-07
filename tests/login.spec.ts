d518eb7cba105fef55c32f4c7bf510032d15fa0b
CyberPhantomX1 <nishayjoshi465@gmail.com>
2026-06-07 22:34:42 +0530

    feat: add negative login test cases

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -14,3 +14,29 @@ test('user can login successfully', async ({ page }) => {
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
     await expect(page.getByText('Products')).toBeVisible();
 });
+
+test('user cannot login with wrong password', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com');
+    await page.getByPlaceholder('Username').fill('standard_user');
+    await page.getByPlaceholder('Password').fill('wrongpassword');
+    await page.locator('[data-test="login-button"]').click();
+    //Error message dikhna chahiye
+    await expect(page.locator('[data-test="error-button"]')).toBeVisible;
+});
+
+test('locked out user cannot login', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+  await page.getByPlaceholder('Username').fill('locked_out_user');
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+
+  // Locked out error dikhna chahiye
+  await expect(page.locator('[data-test="error-button"]')).toBeVisible();
+});
+
+test('user cannot login with empty fields', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com');
+    await page.locator('[data-test="login-button"]').click();
+
+    await expect(page.locator('[data-test="error-button"]')).toBeVisible(); 
+});
